d9f3d70a1d7f92560cf2390ce63e252795852ce3
Srini Sekhar <sekhar4web@gmail.com>
2018-07-29 23:56:20 +0530

    Update app.module.ts

--- app.module.ts
+++ app.module.ts
@@ -1,26 +1,24 @@
+import { Storage } from '@ionic/storage';
+import { FormsModule } from '@angular/forms';
+import { EmiPage } from './../pages/emi/emi';
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
 
 import { HomePage } from '../pages/home/home';
-import { EmiPage } from './../pages/emi/emi';
-import { CalculateEmiPage } from './../pages/calculate-emi/calculate-emi';
 import { HistoryPage } from './../pages/history/history';
 import { LoanComparePage } from './../pages/loan-compare/loan-compare';
 import { ListProfilePage } from './../pages/list-profile/list-profile';
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-import { SessionStorageProvider } from '../providers/session-storage/session-storage';
 import { IonicStorageModule } from '@ionic/storage';
 
 @NgModule({
   declarations: [
     MyApp,    
     HomePage,
-    CalculateEmiPage,
     EmiPage,
     HistoryPage,
     ListProfilePage,
@@ -40,7 +38,6 @@ import { IonicStorageModule } from '@ionic/storage';
   entryComponents: [
     MyApp,
     HomePage,
-    CalculateEmiPage,
     EmiPage,
     HistoryPage,
     ListProfilePage,

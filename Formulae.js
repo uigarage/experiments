b2adbbf0280071ef792711925f40d5f3e184d860
Srini Sekhar <sekhar4web@gmail.com>
2018-08-10 19:58:09 +0530

    Update Formulae.js

--- Formulae.js
+++ Formulae.js
@@ -14,3 +14,18 @@
     
     roi = ( emi / principal ) * ( (Math.pow( (1+r), n) -1) / (Math.pow( (1+r), n) )  );
     console.log("ROI = ", Math.round(roi * 12 * 100));
+
+
+// R = 30/12/100
+// 0.025
+// x = 1 + R
+// 1.025
+// emi = 9749;
+// 9749
+// P = 100000;
+// 100000
+// y = emi / (emi - (P*R))
+// 1.3448751551938198
+// Math.log(y) / Math.log(x)
+// 11.999588388793368
+// Math.round(Math.log(y) / Math.log(x));
